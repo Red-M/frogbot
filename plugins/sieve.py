079cf8866c77c014961269892be2f610939856e7
Red_M <pooatyou.com@gmail.com>
2012-10-13 19:21:51 +1000

    fixes. also removed something that seems to have slightly fixed the mem leak sorta...

--- plugins/sieve.py
+++ plugins/sieve.py
@@ -8,8 +8,8 @@ def sieve_suite(bot, input, func, kind, args):
     ignored = input.conn.conf['ignore']
     if kind == "command":
         if "^" in input.paraml[1]:
-            input.inp.replace("^",bot.chanseen[input.conn.name][input.chan][0])
-            input.paraml[1].replace("^",bot.chanseen[input.conn.name][input.chan][0])
+            input.inp = input.inp.replace("^",bot.chanseen[input.conn.name][input.chan][0])
+            input.paraml[1] = input.paraml[1].replace("^",bot.chanseen[input.conn.name][input.chan][0])
         if input.trigger in bot.config["disabled_commands"]:
             return None
     
@@ -107,36 +107,36 @@ def seen_user(inp, nick='', db=None, input=None, bot=None):
     
 @hook.event('PRIVMSG')
 def chanwatching(inp, input=None, bot=None):
-    if not input.chan in bot.chanseen[input.conn.server]:
-        bot.chanseen[input.conn.server][input.chan]=[]
-        bot.chanseen[input.conn.server][input.chan].append(input.paraml[1])
-    if len(bot.chanseen[input.conn.server][input.chan])==1:
-        bot.chanseen[input.conn.server][input.chan].insert(1, bot.chanseen[input.conn.server][input.chan][0])
+    if not input.chan in bot.chanseen[input.conn.name]:
+        bot.chanseen[input.conn.name][input.chan]=[]
+        bot.chanseen[input.conn.name][input.chan].append(input.paraml[1])
+    if len(bot.chanseen[input.conn.name][input.chan])==1:
+        bot.chanseen[input.conn.name][input.chan].insert(1, bot.chanseen[input.conn.name][input.chan][0])
         time.sleep(.1)
-        bot.chanseen[input.conn.server][input.chan].insert(0, input.paraml[1])
-    bot.chanseen[input.conn.server][input.chan].insert(1, bot.chanseen[input.conn.server][input.chan][0])
+        bot.chanseen[input.conn.name][input.chan].insert(0, input.paraml[1])
+    bot.chanseen[input.conn.name][input.chan].insert(1, bot.chanseen[input.conn.name][input.chan][0])
     time.sleep(.1)
-    bot.chanseen[input.conn.server][input.chan].insert(0, input.paraml[1])
-    id = len(bot.chanseen[input.conn.server][input.chan])
-    for idel in bot.chanseen[input.conn.server][input.chan]:
-        if len(bot.chanseen[input.conn.server][input.chan])>=3:
+    bot.chanseen[input.conn.name][input.chan].insert(0, input.paraml[1])
+    id = len(bot.chanseen[input.conn.name][input.chan])
+    for idel in bot.chanseen[input.conn.name][input.chan]:
+        if len(bot.chanseen[input.conn.name][input.chan])>=3:
             id = id-1
-            bot.chanseen[input.conn.server][input.chan].pop(id)
+            bot.chanseen[input.conn.name][input.chan].pop(id)
     
 @hook.event('PRIVMSG')
 def userwatching(inp, nick='', chan='', input=None, bot=None):
-    if not input.nick in bot.seen[input.conn.server]:
-        bot.seen[input.conn.server][input.nick]=[]
-        bot.seen[input.conn.server][input.nick].append(input.paraml[1])
-    if len(bot.seen[input.conn.server][input.nick])==1:
-        bot.seen[input.conn.server][input.nick].insert(1, bot.seen[input.conn.server][input.nick][0])
+    if not input.nick in bot.seen[input.conn.name]:
+        bot.seen[input.conn.name][input.nick]=[]
+        bot.seen[input.conn.name][input.nick].append(input.paraml[1])
+    if len(bot.seen[input.conn.name][input.nick])==1:
+        bot.seen[input.conn.name][input.nick].insert(1, bot.seen[input.conn.name][input.nick][0])
         time.sleep(.1)
-        bot.seen[input.conn.server][input.nick].insert(0, input.paraml[1])
-    bot.seen[input.conn.server][input.nick].insert(1, bot.seen[input.conn.server][input.nick][0])
+        bot.seen[input.conn.name][input.nick].insert(0, input.paraml[1])
+    bot.seen[input.conn.name][input.nick].insert(1, bot.seen[input.conn.name][input.nick][0])
     time.sleep(.1)
-    bot.seen[input.conn.server][input.nick].insert(0, input.paraml[1])
-    id = len(bot.seen[input.conn.server][input.nick])
-    for idel in bot.seen[input.conn.server][input.nick]:
-        if len(bot.seen[input.conn.server][input.nick])>=3:
+    bot.seen[input.conn.name][input.nick].insert(0, input.paraml[1])
+    id = len(bot.seen[input.conn.name][input.nick])
+    for idel in bot.seen[input.conn.name][input.nick]:
+        if len(bot.seen[input.conn.name][input.nick])>=3:
             id = id-1
-            bot.seen[input.conn.server][input.nick].pop(id)
+            bot.seen[input.conn.name][input.nick].pop(id)
